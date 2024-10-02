da756431fe959f593fe48a4c26579bd06f02df47
dtron <darren@d3tape.com>
2024-10-02 13:16:05 +1300

    next update

--- sandbox.py
+++ sandbox.py
@@ -6,6 +6,13 @@ class Rollnow:
         roll2 = rd.randint(1,6)
         return roll1, roll2
 
+    def rolld20(self):
+        roll1 = rd.randint(1,20)
+        roll2 = rd.randint(1,20)
+        return roll1, roll2
 
-dice = Rollnow()
-print(dice.rolld6())
+d6 = Rollnow()
+print(d6.rolld6())
+
+d20 = Rollnow()
+print(d20.rolld20())
